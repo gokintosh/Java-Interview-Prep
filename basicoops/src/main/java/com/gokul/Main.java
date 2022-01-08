6272b08ade85e89319d7fed55dc99131c1b5fd71
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2022-01-08 12:54:23 +0100

    add lambda

--- basicoops/src/main/java/com/gokul/Main.java
+++ basicoops/src/main/java/com/gokul/Main.java
@@ -1,6 +1,28 @@
 package com.gokul;
 
+import lombok.Data;
+
 public class Main {
 //    why do we need OOP? How do we create 5 students with roll number, name and grade?
-//    class in a named group of properties and functions. That
+//    class in a named group of properties and functions. That virtualizes the real world.
+//    classes are just a group of rule for real life entities called objects.(class->logical construct,object->physical reality)
+
+    public static void main(String[] args) {
+        Student gokul=new Student();
+        gokul.rollno=15;
+        gokul.marks=500;
+        gokul.name="gokul";
+        System.out.println(gokul.toString());
+    }
+
+
+
+
+}
+
+@Data
+class Student{
+    int rollno;
+    String name;
+    float marks;
 }
